2539a90405e84f20f82f43028a6f14663b13f3a9
chandan <chandankumar.btele20@pec.edu.in>
2024-08-09 16:49:48 +0530

    for loops and while loops

---
+++ 9aug/index.js
@@ -0,0 +1,149 @@
+
+// let i=0;
+// let j=0;
+
+// console.log(3<4)
+
+// console.log(3==3)
+
+// console.log(7>8)
+
+
+// console.log(3<4 || 5<4)
+
+// console.log(9<4 || 5<4)
+
+// console.log(8>3 && 8>9)
+
+// console.log(8>3 && 8>4 && 10>9 && 9>11 && 12>11)
+
+
+// let i=0;
+
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+
+// while(i<=10){
+//     console.log(i);
+//     i=i+2
+// }
+
+//question ye hai console multiple of 4 from 0...100
+
+// let i=0
+// while(i<=100){
+//     console.log(i);
+//     i=i+4
+// }
+
+//question ye hai console multiple of 3,5 from 0...100
+
+// let i=0
+// while(i<=100){
+//     if(i%3==0 || i%5==0){
+//         console.log(i);
+       
+//     }
+    
+//     i++
+// }
+    
+
+// while(i<=100){
+//     if(i%3==0 && i%5==0){
+//         console.log(i);
+       
+//     }
+    
+//     i++
+// }
+
+// let i=0;
+// let j=0;
+// let cnt=0;
+
+// while(i<100 && j<50 ){
+//     cnt++;
+//     i++;
+//     j=j+2;
+
+
+// }
+// console.log(cnt)
+
+
+// let i=0;
+// let j=0;
+// let cnt=0;
+
+// while(i<100 || j<50 ){
+//     cnt++;
+//     i++;
+//     j=j+2;
+
+
+// }
+// console.log(cnt)
+
+// Sum of First N Numbers:
+// Use a while loop to calculate the sum of the first n positive integers. For example, if n = 5, the sum would be 1 + 2 + 3 + 4 + 5 = 15
+
+// let n=40
+// // let i=0
+// let sum=0;
+
+// let i=1;
+
+// while(i<=n){
+//     sum=sum+i;
+//     i++
+
+// }
+
+// console.log(sum)
+
+
+// Print Odd Numbers:
+// Write a while loop that prints all odd numbers between 1 and 20.
+
+
+// let i=0
+// while(i<=20){
+//     if(i%2==1){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+let i=0;
+let j=1000000;
+cnt=0
+// j=j/10    i=i+10   .....in while loop
+while(j>=1){
+    // if( i=i+10 ,j=j/10){
+    //     console.log(i)
+    //     console.log(j)
+    // }console.log(cnt)
+    j=j/10
+    cnt++
+}
+
+console.log(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
